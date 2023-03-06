19286ac6a4b66d8ff44e592752f1ce12bdd7da16
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-06 19:41:07 -0300

    Add project files.

---
+++ VerificaPar/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Informe o número a ser verificado: ");
+
+if (!int.TryParse(Console.ReadLine(), out var num))
+{
+    Console.WriteLine("Precisa ser número inteiro!");
+}
+else
+{
+    if (num % 2 == 0)
+    {
+        Console.WriteLine("É Par!");
+    }
+    else
+    {
+        Console.WriteLine("É impar!");
+    }
+}
